3deed730cfd49d3ebaf56ca0870ff0e34d65feaa
~$  The Coder ( nixton ) <83667327+nixtondev@users.noreply.github.com>
2023-07-04 17:30:20 +0200

    Update system.py
    Update large fix.

--- linux/python/system.py
+++ linux/python/system.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
 import subprocess
+import colorama
+from colorama import Fore
+
+ver = "0.1.5"
 
 # Clear screen
 subprocess.run(["clear"])
 
 subprocess.run("curl https://nixton.dev/archive/nixton.txt", shell=True)
 
+print("----------------------------------------------------------------")
+print("")
+print("Version: " + ver)
+print("")
 # Update system
 subprocess.run("sudo apt-get update > /dev/null; echo 'Update system.'", shell=True)
 
@@ -14,11 +22,12 @@ subprocess.run("sleep 5", shell=True)
 
 # Upgrade system
 subprocess.run("sudo apt-get upgrade -y > /dev/null; echo 'Upgrade system.'", shell=True)
-
+print("")
 subprocess.run("sleep 5", shell=True)
 
-print("Advanced Package Tool: Showing details.")
-
+print(Fore.RED + "----------------------------------------------------------------")
+print(Fore.RED + "Advanced Package Tool: Showing details.")
+print("----------------------------------------------------------------")
 subprocess.run("sleep 5", shell=True)
 
 # Update System
@@ -30,5 +39,6 @@ subprocess.run("sleep 5", shell=True)
 subprocess.run(["sudo", "apt", "upgrade", "-y"])
 
 subprocess.run("sleep 5", shell=True)
-
-print("[ Done ]")
+print("")
+print(Fore.GREEN + "[ Done ]")
+print("")
